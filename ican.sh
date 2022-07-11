e5ae9b63133deec3081a71464f2563cd810bd3b9
Benjamin Bradley <29411864+notjustanyben@users.noreply.github.com>
2022-07-11 14:13:12 -0400

    Update ican.sh

--- ican.sh
+++ ican.sh
@@ -1,16 +1,22 @@
+# The below code uses enp0s25 as the default name for ethernet adapter when extracting local MAC address per the systemd naming scheme
+# change this adapter name to whatever your local ethernet adapter name is as needed
+
 #!/bin/bash
 
-ipv4="$(curl -s http://ipv4.icanhazip.com)" 
+ipv4="$(curl -s http://ipv4.icanhazip.com)"
 ipv6="$(curl -s http://ipv6.icanhazip.com)"
-sleep 0.25
+sleep 0.10
 localip="$(hostname -I | awk '{print $1}')"
+localmac="$(ip link show enp0s25 | awk '/ether/ {print $2}')" # replace enp0s25 with your local ethernet adapter's name or leave as default
 localdevices="$(arp -a | wc -l | awk '{print $1}')"
-echo "Your public IPv4 is: $ipv4"
-sleep 0.25
-echo "Your public IPv6 is: $ipv6"
-sleep 0.25
-echo "Your local IP is: $localip"
-sleep 0.25
-echo "You appear to have [$localdevices] device(s) on your local network"
-sleep 0.75
-echo "Thank you for coming to my TED Talk"
+echo "Your public IPv4 address is: $ipv4"
+sleep 0.10
+echo "Your public IPv6 address is: $ipv6"
+sleep 0.10
+echo "Your local IP address is: $localip"
+sleep 0.10
+echo "Your local MAC address is: $localmac"
+sleep 0.10
+echo "There appears to be [$localdevices] device(s) on your local network"
+sleep .10
+
